1ff8407a123ba419a6e355e2651de1e26ef9486a
zicheng <zicheng@C010423383.zipangu.cac.co.jp>
2026-04-16 12:15:56 +0900

    基礎編すべて完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -11,6 +13,15 @@ public class Question02_2 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		System.out.println("商品の名前と値段を入力してください。");
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		String itemName = reader.readLine();
+		int itemPrice = Integer.parseInt(reader.readLine());
+
+		System.out.println("商品の名前は%sです。 \n".formatted(itemName)
+				+ "商品の値段は %d 円です。".formatted(itemPrice));
 
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
@@ -1,6 +1,8 @@
 package basic.question04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第7章 配列
@@ -10,8 +12,25 @@ public class Question04_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		int[] ageGroup = new int[6];
+
+		ageGroup[0] = 23;
+		ageGroup[1] = 33;
+		ageGroup[2] = 26;
+		ageGroup[3] = 21;
+		ageGroup[4] = 25;
+		ageGroup[5] = 22;
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		System.out.println("4 番目の人の年齢を入力してください。 ");
+		int temporaryAge = Integer.parseInt(reader.readLine());
+		ageGroup[3] = temporaryAge;
+
+		System.out.println("4 番目の人の年齢は%d歳です。".formatted(ageGroup[3]));
+
+		System.out.println("人数は%d人です。 ".formatted(ageGroup.length));
 	}
 
 }
